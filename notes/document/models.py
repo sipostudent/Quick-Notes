23db7a37c27dc93afab8f25c9e59f3d9bcafedaa
Sipo Charles <sipocharles@yahoo.co.uk>
2020-10-11 11:31:38 +0100

    Save, Add, Delete Functionality

--- notes/document/models.py
+++ notes/document/models.py
@@ -8,5 +8,8 @@ class Document(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True, null=True)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         ordering = ('title',)
--- notes/notes/urls.py
+++ notes/notes/urls.py
@@ -19,9 +19,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from document.views import editor
+from document.views import editor, delete_document
 
 urlpatterns = [
     path('', editor, name='editor'),
+    path('delete_document/<int:docid>/', delete_document, name='delete_document'),
     path('admin/', admin.site.urls),
 ]
